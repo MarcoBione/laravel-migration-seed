6d75851a938f4b4af8740121c4a3ef97090c217a
MarcoBione <marcobione.work@gmail.com>
2023-05-24 11:36:42 +0200

    Update database table + new migration for descript

--- database/migrations/2023_05_24_082802_create_houses_table.php
+++ database/migrations/2023_05_24_082802_create_houses_table.php
@@ -15,7 +15,16 @@ return new class extends Migration
     {
         Schema::create('houses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
+            $table->string('address',100);
+            $table->string('postal_code',10);
+            $table->string('city',50);
+            $table->string('state',50);
+            $table->smallInteger('square_meters')->unsigned();
+            $table->tinyInteger('rooms')->unsigned();
+            $table->tinyInteger('bathroom')->unsigned();
+            $table->boolean('garage')->default(0);
+            $table->float('price',10,2);
             $table->timestamps();
         });
     }
